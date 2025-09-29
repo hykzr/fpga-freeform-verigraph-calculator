dda85b85f32e60641c84e3dd590332d7b39c9ec7
hykzr <phhhhhhhhh135@gmail.com>
2025-09-29 09:49:05 +0800

    initial commit

---
+++ FDP.srcs/sources_1/new/clock_devider.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 29.09.2025 09:20:14
+// Design Name: 
+// Module Name: clock_devider
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module clock_devider(
+    input clock,
+    output clock_6hz
+    );
+    
+endmodule
